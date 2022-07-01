51feaee983cf437bf955cfddbc1af6c16928c42d
Miconintel <miconintelligence@gmail.com>
2022-07-01 09:49:05 +0100

    upto search working

---
+++ public/js/search.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+
+export const searchFood = async (product)=>{
+    try{
+        const res = await axios(`/api/v1/foods/?category=${product}`)
+      const model = res.data.data
+        console.log (model)
+    }catch(err){
+        console.log(err)
+    }
+}
