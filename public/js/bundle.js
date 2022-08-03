0aba793d254bbde9f2694e3ec6f96db6c80d795e
Miconintel <miconintelligence@gmail.com>
2022-08-03 13:28:54 +0100

    l

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -10065,7 +10065,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57719" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62179" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
